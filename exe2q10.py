aa1d3285635d63172f383c8568c8b7667dd43936
thaisom <thais_tmj@hotmail.com>
2017-07-18 20:39:41 -0300

    Add files via upload

---
+++ exe2q10.py
@@ -0,0 +1,5 @@
+#Escreva um algoritmo/programa que mostre o triplo de um número informados pelo usuário.
+def triplo(x):
+    return 3*x
+x = int(input('Digite um número:'))
+print('O triplo de',x,'é',triplo(x))
---
+++ exe2q11.py
@@ -0,0 +1,7 @@
+#Recebe nome, endereço e telefone e imprima na tela.
+def nome_end_tel(n,e,t):
+    return n,e,t
+n = input("Qual o seu nome?:")
+e = input("Qual seu endereço?:")
+t = input("Qual seu telefone?:")
+print(nome_end_tel(n,e,t))

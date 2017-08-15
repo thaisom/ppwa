8d34c9c9b336ea6dee65f232f024f31ae77d28fb
thaisom <thais_tmj@hotmail.com>
2017-08-15 17:49:21 -0300

    Add files via upload

---
+++ exe4q6.py
@@ -0,0 +1,9 @@
+#06
+
+user = input("Digite usuario:")
+psw = input("Digite a senha:")
+
+while user == psw :
+    print("Os valores não podem ser iguais!")
+    user = input("Digite usuário:")
+    psw = input("Digite a senha:")
